229e32efd633e528596f10761d121c55a30217d6
DamianCG00 <marco.coba110107@gmail.com>
2026-06-02 11:44:38 -0600

    feat: implement ProductoRepository and custom query methods - Add custom query methods in ProductoCrudRepository (sorted category filter and low stock check).- Create ProductoRepository to wrap CRUD operations (getAll, getByCategoria, getEscasos, getProductoById, save).

--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/persistence/crud/ProductoCrudRepository.java
@@ -3,10 +3,27 @@ package mx.edu.tecdesoftware.market_backend_2026_3_b.persistence.crud;
 import mx.edu.tecdesoftware.market_backend_2026_3_b.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository  extends CrudRepository<Producto, Integer> {
 
+    //obtener la lista de productos filtrados por id  de categoria y ordenados ascendentemente por nombre
 
-    }
+        /* Select *
+        from Categorias
+        WHERE id_categoria = ?
+        ORDER BY Nombre ASC
+
+         */
+
+
+    List<Producto> findByCantidadOrderByNombre(int idCategoria);
+
+
+    //Obtener las productos
+    Optional<List<Producto>> findByCantidadStockLessThanAndEstado(int cantidad, boolean estado);
+}
 
 
 
