011b472592e031b78d4de6590436c2fc41918e2f
DamianCG00 <marco.coba110107@gmail.com>
2026-06-02 17:19:02 -0600

    feat2: implement ProductoRepository and custom query methods - Add custom query methods in ProductoCrudRepository (sorted category filter and low stock check).- Create ProductoRepository to wrap CRUD operations (getAll, getByCategoria, getEscasos, getProductoById, save).

--- src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/persistence/ProductoRepository.java
+++ src/main/java/mx/edu/tecdesoftware/market_backend_2026_3_b/persistence/ProductoRepository.java
@@ -1,18 +1,38 @@
 package mx.edu.tecdesoftware.market_backend_2026_3_b.persistence;
 
-import mx.edu.tecdesoftware.market_backend_2026_3_b.persistence.crud.ProductoCrudRepository;
+import java.util.List;
+
+import mx.edu.tecdesoftware.market_backend_2026_3_b.persistence.crud.IProductoCrudRepository;
 import mx.edu.tecdesoftware.market_backend_2026_3_b.persistence.entity.Producto;
 
-import  java.util.*;
+import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
+    public IProductoCrudRepository productoCrudRepository;
 
-    private ProductoCrudRepository productoCrudRepository;
-
-
-    //SELECT * FORM productos
     public List<Producto> getAll() {
-        //Se "castea" Iterable a lista
+        //Se "castea" Iterable a la lista
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+
+    public List<Producto> getByCategoria(int idCategoria) {
+        return productoCrudRepository.findByCantidadOrderByNombreAsc(idCategoria);
+    }
+
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStockLessThanAndEstado(cantidad, true);
+    }
+
+    public Optional<Producto> getById(int idProducto) {
+        return productoCrudRepository.findById(idProducto);
+    }
+
+    public Producto addProducto(Producto producto) {
+        return productoCrudRepository.save(producto);
+    }
+
+    public void deleteProducto(int idProducto) {
+        productoCrudRepository.deleteById(idProducto);
     }
 }
